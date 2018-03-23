cf781756b10aed4be1d5de5681c66afabfb8dfe3
Jim Reid <reidj32@gmail.com>
2018-03-23 16:05:29 -0500

    Refactored startup class

--- Library.Api/Startup.cs
+++ Library.Api/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using AspNetCoreRateLimit;
 using Library.Api.Entities;
 using Library.Api.Helpers;
 using Library.Api.Models;
@@ -18,8 +18,9 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using NLog.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 using System.Linq;
-using AspNetCoreRateLimit;
 
 namespace Library.Api
 {
@@ -34,94 +35,141 @@ namespace Library.Api
             _configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container. For more information
-        // on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        // ReSharper disable once UnusedMember.Global
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(setupAction =>
-            {
-                setupAction.ReturnHttpNotAcceptable = true;
-                setupAction.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
-
-                XmlDataContractSerializerInputFormatter xmlDataContractSerializerInputFormatter =
-                    new XmlDataContractSerializerInputFormatter();
-
-                xmlDataContractSerializerInputFormatter.SupportedMediaTypes.Add(
-                    "application/vnd.marvin.authorwithdateofdeath.full+xml");
-
-                setupAction.InputFormatters.Add(xmlDataContractSerializerInputFormatter);
-
-                JsonOutputFormatter jsonOutputFormatter =
-                    setupAction.OutputFormatters.OfType<JsonOutputFormatter>().FirstOrDefault();
-
-                jsonOutputFormatter?.SupportedMediaTypes.Add(VendorMediaType);
-
-                JsonInputFormatter jsonInputFormatter =
-                    setupAction.InputFormatters.OfType<JsonInputFormatter>().FirstOrDefault();
-
-                jsonInputFormatter?.SupportedMediaTypes.Add("application/vnd.marvin.author.full+json");
-                jsonInputFormatter?.SupportedMediaTypes.Add("application/vnd.marvin.authorwithdateofdeath.full+json");
-            }).AddJsonOptions(options =>
-            {
-                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-            });
-
-            services.AddHttpCacheHeaders(expirationModelOptions =>
-                {
-                    expirationModelOptions.MaxAge = 600;
-                },
-                validationModelOptions =>
-                {
-                    validationModelOptions.AddMustRevalidate = true;
-                });
-
+            services.AddMvc(SetupMvcOptions).AddJsonOptions(SetupMvcJsonOption);
+            services.AddHttpCacheHeaders(SetExpireCacheHeaders, SetupValidationCacheHeaders);
             services.AddResponseCaching();
-
             services.AddMemoryCache();
-            services.Configure<IpRateLimitOptions>(options =>
-            {
-                options.GeneralRules = new List<RateLimitRule>
-                {
-                    new RateLimitRule
-                    {
-                        Endpoint = "*",
-                        Limit = 1000,
-                        Period = "5m"
-                    },
-                    new RateLimitRule
-                    {
-                        Endpoint = "*",
-                        Limit = 200,
-                        Period = "10s"
-                    }
-                };
-            });
+            services.Configure<IpRateLimitOptions>(SetupIpRateLimitOptions);
 
-            string connectionString = _configuration["ConnectionStrings:LibraryDB"];
-            services.AddDbContext<LibraryContext>(o => o.UseSqlServer(connectionString));
-
+            services.AddDbContext<LibraryContext>(SetupDbContext);
             services.AddScoped<ILibraryRepository, LibraryRepository>();
-
-            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
-
-            services.AddScoped<IUrlHelper, UrlHelper>(factory =>
-                new UrlHelper(factory.GetService<IActionContextAccessor>().ActionContext));
-
+            services.AddScoped<IUrlHelper, UrlHelper>(SetupUrlHelper);
             services.AddTransient<IPropertyMappingService, PropertyMappingService>();
-
             services.AddTransient<ITypeHelperService, TypeHelperService>();
 
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
             services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
             services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // ReSharper disable once UnusedMember.Global
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory,
             LibraryContext libraryContext)
         {
             loggerFactory.AddNLog();
 
+            ConfigureExceptionHandling(app, env, loggerFactory);
+            ConfigureObjectMapping();
+
+            libraryContext.Database.Migrate();
+            libraryContext.EnsureSeedDataForContext();
+
+            app.UseIpRateLimiting();
+            app.UseResponseCaching();
+            app.UseHttpCacheHeaders();
+            app.UseMvc();
+        }
+
+        private static void SetupMvcOptions(MvcOptions setupAction)
+        {
+            setupAction.ReturnHttpNotAcceptable = true;
+            setupAction.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+
+            XmlDataContractSerializerInputFormatter xmlDataContractSerializerInputFormatter =
+                new XmlDataContractSerializerInputFormatter();
+
+            xmlDataContractSerializerInputFormatter.SupportedMediaTypes.Add(
+                "application/vnd.marvin.authorwithdateofdeath.full+xml");
+
+            setupAction.InputFormatters.Add(xmlDataContractSerializerInputFormatter);
+
+            JsonOutputFormatter jsonOutputFormatter =
+                setupAction.OutputFormatters.OfType<JsonOutputFormatter>().FirstOrDefault();
+
+            jsonOutputFormatter?.SupportedMediaTypes.Add(VendorMediaType);
+
+            JsonInputFormatter jsonInputFormatter =
+                setupAction.InputFormatters.OfType<JsonInputFormatter>().FirstOrDefault();
+
+            jsonInputFormatter?.SupportedMediaTypes.Add("application/vnd.marvin.author.full+json");
+            jsonInputFormatter?.SupportedMediaTypes.Add("application/vnd.marvin.authorwithdateofdeath.full+json");
+        }
+
+        private static void SetupMvcJsonOption(MvcJsonOptions options)
+        {
+            options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+            JsonConvert.DefaultSettings = () => options.SerializerSettings;
+        }
+
+        private static UrlHelper SetupUrlHelper(IServiceProvider factory)
+        {
+            return new UrlHelper(factory.GetService<IActionContextAccessor>().ActionContext);
+        }
+
+        private static void SetupDbContext(DbContextOptionsBuilder o)
+        {
+            string connectionString = _configuration["ConnectionStrings:LibraryDB"];
+            o.UseSqlServer(connectionString);
+        }
+
+        private static void SetupIpRateLimitOptions(IpRateLimitOptions options)
+        {
+            options.GeneralRules = new List<RateLimitRule>
+            {
+                new RateLimitRule
+                {
+                    Endpoint = "*",
+                    Limit = 1000,
+                    Period = "5m"
+                },
+                new RateLimitRule
+                {
+                    Endpoint = "*",
+                    Limit = 200,
+                    Period = "10s"
+                }
+            };
+        }
+
+        private static void SetupValidationCacheHeaders(Marvin.Cache.Headers.ValidationModelOptions validationModelOptions)
+        {
+            validationModelOptions.AddMustRevalidate = true;
+        }
+
+        private static void SetExpireCacheHeaders(Marvin.Cache.Headers.ExpirationModelOptions expirationModelOptions)
+        {
+            expirationModelOptions.MaxAge = 600;
+        }
+
+        private static void ConfigureObjectMapping()
+        {
+            AutoMapper.Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Author, AuthorDto>()
+                    .ForMember(dest => dest.Name, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+                    .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge(src.DateOfDeath)));
+
+                cfg.CreateMap<Book, BookDto>();
+
+                cfg.CreateMap<AuthorForCreationDto, Author>();
+
+                cfg.CreateMap<AuthorForCreationWithDateOfDeathDto, Author>();
+
+                cfg.CreateMap<BookForCreationDto, Book>();
+
+                cfg.CreateMap<BookForUpdateDto, Book>();
+
+                cfg.CreateMap<Book, BookForUpdateDto>();
+            });
+        }
+
+        private static void ConfigureExceptionHandling(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -146,39 +194,6 @@ namespace Library.Api
                     });
                 });
             }
-
-            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
-            {
-                ContractResolver = new CamelCasePropertyNamesContractResolver(),
-                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-            };
-
-            AutoMapper.Mapper.Initialize(cfg =>
-            {
-                cfg.CreateMap<Author, AuthorDto>()
-                    .ForMember(dest => dest.Name, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
-                    .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge(src.DateOfDeath)));
-
-                cfg.CreateMap<Book, BookDto>();
-
-                cfg.CreateMap<AuthorForCreationDto, Author>();
-
-                cfg.CreateMap<AuthorForCreationWithDateOfDeathDto, Author>();
-
-                cfg.CreateMap<BookForCreationDto, Book>();
-
-                cfg.CreateMap<BookForUpdateDto, Book>();
-
-                cfg.CreateMap<Book, BookForUpdateDto>();
-            });
-
-            libraryContext.Database.Migrate();
-            libraryContext.EnsureSeedDataForContext();
-
-            app.UseIpRateLimiting();
-            app.UseResponseCaching();
-            app.UseHttpCacheHeaders();
-            app.UseMvc();
         }
     }
 }
